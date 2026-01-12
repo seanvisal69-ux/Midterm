bdfb0eb7cd847334ac64e09fcb63a9e3a55b1a26
seanvisal69-ux <seanvisal69gmail.com>
2026-01-12 10:28:40 +0700

    week2

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include'./includes/header.inc.php';
+include'./includes/navbar.inc.php';
+
+$available_pages = ['login', 'register'];
+
+
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+    if (in_array($page, $available_pages)){
+        include './pages/'.$page.'.php';
+    }else{
+        echo '<h1>Error 404</h1>';
+    }
+
+}else{
+    echo '<h1>Home page</h1>';
+}
+
+
+
+include'./includes/footer.inc.php';
+?>
