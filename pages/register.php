44bc101b69c6aa9c337197fa79ae065c91c93150
seanvisal69-ux <seanvisal69gmail.com>
2026-01-19 10:08:17 +0700

    week3

--- pages/register.php
+++ pages/register.php
@@ -1,4 +1,6 @@
 <?php
+    $nameErr = $usernameErr = $passwdErr = $confirm_passwdErr = '';
+    $name = $username = '';
     if(isset($_POST['name'], $_POST['username'], $_POST['passwd'], $_POST['confirm_passwd'])){
         $name = $_POST['name'];
         $username = $_POST['username'];
@@ -26,19 +28,31 @@
         <h3>Register page</h3>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Name</label>
-            <input name="name" type="text" class="form-control">
+            <input name="name" type="text" value="<?php echo $name ?>" class="form-control
+            <?php echo empty($nameErr) ? '' : 'is-invalid'; ?>">
+            <div class="invalid-feedback">
+                <?php echo $nameErr ?>
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Username</label>
-            <input name="username" type="text" class="form-control">
+            <input name="username" type="text" value="<?php echo $username ?>" class="form-control
+            <?php echo empty($usernameErr) ? '' : 'is-invalid'; ?>">
+            <div class="invalid-feedback">
+                <?php echo $usernameErr ?>
         </div>
         <div class="mb-3">
             <label class="form-label">Password</label>
-            <input name="passwd" type="password" class="form-control" >
+            <input name="passwd" type="password" class="form-control
+            <?php echo empty($passwdErr) ? '' : 'is-invalid'; ?>">
+            <div class="invalid-feedback">
+                <?php echo $passwdErr ?>
         </div>
         <div class="mb-3">
             <label class="form-label">Confirm Password</label>
-            <input name="confirm_passwd" type="password" class="form-control" >
+            <input name="confirm_passwd" type="password" class="form-control
+            <?php echo empty($confirm_passwdErr) ? '' : 'is-invalid'; ?>">
+            <div class="invalid-feedback">
+                <?php echo $confirm_passwdErr ?>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
